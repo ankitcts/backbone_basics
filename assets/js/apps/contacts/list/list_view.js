03c52a13c7a340cc875f6efebc497e769055b9a8
Ankit Khandelwal <ankitcts@gmail.com>
2016-06-22 22:36:14 +0530

    Composite View

--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -1,12 +1,23 @@
 ContactManager.module("ContactsApp.List", function (List, ContactManager,
     Backbone, Marionette, $, _) {
     List.Contact = Marionette.ItemView.extend({
-        tagName: "li",
+        tagName: "tr",
         template: Handlebars.compile($("#contact-list-item").html()),
+        events: {
+            "click": "highlightName"
+        },
+
+        highlightName: function (e) {
+            e.preventDefault();
+            this.$el.toggleClass("warning");
+        }
     });
 
-    List.Contacts = Marionette.CollectionView.extend({
-        tagName: "ul",
-        childView: List.Contact
+    List.Contacts = Marionette.CompositeView.extend({
+        tagName: "table",
+        className: "table table-hover",
+        childView: List.Contact,
+        template: Handlebars.compile($("#contact-list").html()),
+        itemViewContainer: "tbody"
     });
 });

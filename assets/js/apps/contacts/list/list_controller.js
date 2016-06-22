2ca3acaae5ceb8335ea27526cdf29c7ff3bbe94d
Ankit Khandelwal <ankitcts@gmail.com>
2016-06-23 00:59:46 +0530

    Routing

--- assets/js/apps/contacts/list/list_controller.js
+++ assets/js/apps/contacts/list/list_controller.js
@@ -9,7 +9,7 @@ ContactManager.module("ContactsApp.List", function (List, ContactManager,
             });
             contactsListView.on("childview:contact:show",
                 function (childView, model) {
-                    ContactManager.ContactsApp.Show.Controller.showContact(model);
+                    ContactManager.trigger("contact:show", model.get("id"));
                 });
             contactsListView.on("childview:contact:delete", function (childView, model) {
                 contacts.remove(model);

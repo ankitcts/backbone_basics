aab4ac4e2a8520a404aeff7f3e47cfa69e5a7717
Ankit Khandelwal <ankitcts@gmail.com>
2016-06-22 22:22:56 +0530

    Creating sub-application

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,27 +5,5 @@ ContactManager.addRegions({
 });
 
 ContactManager.on("start", function () {
-   var contacts = ContactManager.request("contact:entities");
-
-    var listView = new ContactManager.ContactsView({
-        collection: contacts
-    })
-    ContactManager.mainRegion.show(listView);
-});
-
-
-ContactManager.ContactItemView = Marionette.ItemView.extend({
-    template: Handlebars.compile($("#contact-list-item").html()),
-    tagName: "li",
-    events: {
-        "click p": "_alertPhone"
-    },
-    _alertPhone: function () {
-        alert(this.model.get("phoneNumber"));
-    }
-});
-
-ContactManager.ContactsView = Marionette.CollectionView.extend({
-    tagName: "ul",
-    childView: ContactManager.ContactItemView
+    ContactManager.ContactsApp.List.Controller.listContacts();
 });
